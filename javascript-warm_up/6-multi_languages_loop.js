4750223d82b5e80631e5223609b19d7005516ba9
Geewynxofficial <godwinning247@gmail.com>
2023-09-05 01:21:37 +0100

    my name

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myName = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const name of myName) {
+for (let i = 0; i < lines.length; i++) {
     console.log(myName);
 }
