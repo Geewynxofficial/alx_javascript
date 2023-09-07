5561f16d9d82fc54d9dd53a4effa3197a5d6fffa
Geewynxofficial <godwinning247@gmail.com>
2023-09-07 20:55:16 +0100

    funct

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 module.exports.add = function add(num1, num2) {
-  const sum = num1 + num2;
-  return sum;
-};
-// const add = require('./13-add').add;
-console.log(add(4, 85));
+  console.log(add(4, 85));
+  
+}
