4844a621452da212bfed485713b98dbbebd2704b
Geewynxofficial <godwinning247@gmail.com>
2023-09-07 17:07:06 +0100

    replace

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const myObject = ['C is fun', 'Python is cool', 'JavaScript is amazing', 'value: 12'];
-myObject.splice(3, 1, 89);
+myObject.splice(3, 1, 'value: 89');
 console.log(myObject);
