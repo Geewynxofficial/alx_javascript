f7fe7e1afb40017ab52fce62c879e1f83dd022bd
Geewynxofficial <godwinning247@gmail.com>
2023-09-07 17:41:17 +0100

    replace

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
-const myObject = ["value: 12"];
-for (let i = 0; i < myObject.length; i++) {
-    if (myObject[i] === 12) {
-        myObject[i] = ["value: 12"];
+const myObjects = ["value: 12"];
+const objects = [];
+for (const myObject of myObjects) {
+    const obj = {
+        "type": "object",
+        "value": myObject,
+    }
+}
+for (let i = 0; i < myObjects.length; i++) {
+    if (myObjects[i] === 12) {
+        myObjects[i] = ["value: 12"];
     }
 }
 console.log(myObject);
