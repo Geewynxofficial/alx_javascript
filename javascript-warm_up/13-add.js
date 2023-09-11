123fad9af08fd27f218b930101a8d507c861a913
Geewynxofficial <godwinning247@gmail.com>
2023-09-11 20:00:50 +0100

    function

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function add(num1, num2){
-  return num1 + num2;
+function add(number1, number2){
+  return number1 + number2;
 };
+
 console.log(add(4, 85));
-const number = add(4, 85);
-console.log(number);
+
 
