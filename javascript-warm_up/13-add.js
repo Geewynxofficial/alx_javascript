00abaf5c07df9740f1852608adb584d1358e111c
Geewynxofficial <godwinning247@gmail.com>
2023-09-11 20:23:40 +0100

    summing

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-function add(number1, number2){
+function add(number1, number2) {
   return number1 + number2;
 }
-
-console.log(add(4, 85));
-console.log(add(93, -4));
-console.log(add(0, 89));
+add(4, 85);
+add(93, -4);
+add(0, 89);
