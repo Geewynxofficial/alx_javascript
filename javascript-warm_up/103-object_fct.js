bf27f09aa1aa9c672dc63887c90ced910a3fe4e4
Geewynxofficial <godwinning247@gmail.com>
2023-09-13 19:35:10 +0100

    increment

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,6 +1,16 @@
+function incr(value) {
+  return value + 1;
+}
 
-function incr() {
-  this.value++;
+ myObject = {
+    type: "object", 
+    value: 12,
+    incr: incr
+};
+
+
+for (let i = 0; i < 3; i++){
+myObject.value = incr(myObject.value);
 }
 
 module.exports = {myObject};
