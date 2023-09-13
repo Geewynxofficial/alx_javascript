de13a8c8d8412c143b61fb5e51296ba44da80696
Geewynxofficial <godwinning247@gmail.com>
2023-09-13 13:41:51 +0100

    update

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,12 +1,14 @@
+
 let myObject = {
     type: "object", 
     value: 12
 };
-
 function incr(value) {
-  incr++;
+  myObject++;
   value();
 }
+
+
 incr(myObject.value);
 
 module.exports = {myObject};
