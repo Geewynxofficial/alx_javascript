b5947cbaee0f9d3de91c57a9d76ea1e9a90a6c37
Geewynxofficial <godwinning247@gmail.com>
2023-09-23 15:18:04 +0100

    Squares

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+class Rectangle {
+    constructor (height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    area () {
+        return this.height * this.width;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size)
+    }
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X';
+        }
+        for (let i = 0; i < this.height; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                row += c;
+            }
+            console.log(row);
+        }
+    }
+}
+module.exports = Square;
