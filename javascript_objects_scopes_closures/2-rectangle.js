411437577b9386fb693dadda330d7715815fd002
Geewynxofficial <godwinning247@gmail.com>
2023-09-23 14:40:08 +0100

    correct rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
 class Rectangle {
     constructor(w, h) {
         if ( w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
