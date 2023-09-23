d53633532bca7701d5586c0d3ec0d90ecb897565
Geewynxofficial <godwinning247@gmail.com>
2023-09-23 01:07:36 +0100

    width and heighth

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -4,21 +4,4 @@ class Rectangle {
         this.height = h;
     }
 }
-
-const r1 = new Rectangle(3, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(3, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(3);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-module.exports = {r1};
-module.exports = {r2};
-module.exports = {r3};
+module.exports = Rectangle;
