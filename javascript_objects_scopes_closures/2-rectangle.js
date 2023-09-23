c7d839f6080e6ffbbb13c81baefe4d0877c2eba3
Geewynxofficial <godwinning247@gmail.com>
2023-09-23 13:52:42 +0100

    rect

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const Rectangle = require('./2-rectangle');
 class Rectangle {
     constructor(w, h) {
         if ( w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
